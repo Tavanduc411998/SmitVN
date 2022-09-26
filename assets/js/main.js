70a128ae54411d0373073562e106407fec076cbf
Tavanduc411998 <57761776+Tavanduc411998@users.noreply.github.com>
2022-09-27 01:16:52 +0700

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 var latestCheckOpening = false
 var oldCheckOpening = false
 
+var featureOpening = false
+
 function showIntro() {
     document.getElementById('content__intro').style.display = 'block'
     document.getElementById('content__vers').style.display = 'none'
@@ -34,3 +36,101 @@ function oldVerClick() {
         oldCheckOpening = true
     }
 }
+
+function feature1Click() {
+    if (featureOpening) {
+        document.getElementById('feature1__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature1__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
+function feature2Click() {
+    if (featureOpening) {
+        document.getElementById('feature2__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature2__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
+
+function feature3Click() {
+    if (featureOpening) {
+        document.getElementById('feature3__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature3__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
+
+function feature4Click() {
+    if (featureOpening) {
+        document.getElementById('feature4__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature4__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
+
+function feature5Click() {
+    if (featureOpening) {
+        document.getElementById('feature5__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature5__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
+
+function feature6Click() {
+    if (featureOpening) {
+        document.getElementById('feature6__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature6__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
+
+function feature7Click() {
+    if (featureOpening) {
+        document.getElementById('feature7__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature7__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
+
+function feature8Click() {
+    if (featureOpening) {
+        document.getElementById('feature8__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature8__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
+
+function feature9Click() {
+    if (featureOpening) {
+        document.getElementById('feature9__box').classList.remove('ver__box--click')
+        featureOpening = false
+    } else {
+        document.getElementById('feature9__box').classList.add('ver__box--click')
+        featureOpening = true
+    }
+}
+
